870a4e43da4b0a27dc0a4cfc4289975ab6444427
PSNA User <user@example.com>
2026-01-26 11:56:52 +0530

    Fix: correctly set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
     plugins: [react()],
-    base: './',
+    base: '/PSNA-TIMETABLES/',
 }))
